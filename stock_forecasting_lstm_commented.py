8aa900ac84ac1710e755d2a70ca9be363e543acf
Maitrri Jayant Chandra <74445547+Maitrri@users.noreply.github.com>
2025-07-30 00:58:53 -0700

    Add files via upload

---
+++ stock_forecasting_lstm_commented.py
@@ -0,0 +1,165 @@
+# Imports
+from tensorflow import keras 
+# Import required libraries
+import pandas as pd 
+# Import required libraries
+import numpy as np 
+from sklearn.preprocessing import StandardScaler
+# Import required libraries
+import matplotlib.pyplot as plt 
+# Import required libraries
+import seaborn as sns 
+# Import required libraries
+import os 
+from datetime import datetime
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+
+data = pd.read_csv("MicrosoftStock.csv")
+# Show first few rows of the dataset
+print(data.head())
+print(data.info())
+print(data.describe())
+
+
+# Initial Data Visualization
+# Plot 1 - Open and Close Prices of time
+# Visualize actual vs predicted prices
+plt.figure(figsize=(12,6))
+# Plot closing price to visualize trend
+plt.plot(data['date'], data['open'], label="Open",color="blue")
+# Plot closing price to visualize trend
+plt.plot(data['date'], data['close'], label="Close",color="red")
+plt.title("Open-Close Price over Time")
+plt.legend()
+# plt.show()
+
+# Plot 2 - Trading Volume (check for outliers)
+# Visualize actual vs predicted prices
+plt.figure(figsize=(12,6))
+# Plot closing price to visualize trend
+plt.plot(data['date'],data['volume'],label="Volume",color="orange")
+plt.title("Stock Volume over Time")
+# plt.show()
+
+
+# Drop non-numeric columns
+numeric_data = data.select_dtypes(include=["int64","float64"])
+
+# Plot 3 - Check for correlation between features
+# Visualize actual vs predicted prices
+plt.figure(figsize=(8,6))
+sns.heatmap(numeric_data.corr(), annot=True, cmap="coolwarm")
+plt.title("Feature Correlation Heatmap")
+# plt.show()
+
+
+# Convert the Data into Date time then create a date filter
+data['date'] = pd.to_datetime(data['date'])
+
+prediction = data.loc[
+    (data['date'] > datetime(2013,1,1)) &
+    (data['date'] < datetime(2018,1,1))
+]
+
+# Visualize actual vs predicted prices
+plt.figure(figsize=(12,6))
+# Plot closing price to visualize trend
+plt.plot(data['date'], data['close'],color="blue")
+plt.xlabel("Date")
+plt.ylabel("Close")
+plt.title("Price over time")
+
+
+# Prepare for the LSTM Model (Sequential)
+stock_close = data.filter(["close"])
+dataset = stock_close.values #convert to numpy array
+training_data_len = int(np.ceil(len(dataset) * 0.95))
+
+# Preprocessing Stages
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(dataset)
+
+training_data = scaled_data[:training_data_len] #95% of all out data
+
+X_train, y_train = [], []
+
+
+# Create a sliding window for our stock (60 days)
+for i in range(60, len(training_data)):
+    X_train.append(training_data[i-60:i, 0])
+    y_train.append(training_data[i,0])
+    
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+
+# Build the Model
+# Define the LSTM model architecture
+model = keras.models.Sequential()
+
+# First Layer
+model.add(keras.layers.LSTM(64, return_sequences=True, input_shape=(X_train.shape[1],1)))
+
+# Second Layer
+model.add(keras.layers.LSTM(64, return_sequences=False))
+
+# 3rd Layer (Dense)
+model.add(keras.layers.Dense(128, activation="relu"))
+
+# 4th Layer (Dropout)
+model.add(keras.layers.Dropout(0.5))
+
+# Final Output Layer
+model.add(keras.layers.Dense(1))
+
+model.summary()
+model.compile(optimizer="adam",
+              loss="mae",
+              metrics=[keras.metrics.RootMeanSquaredError()])
+
+
+# Train the model on the training dataset
+training = model.fit(X_train, y_train, epochs=20, batch_size=32)
+
+# Prep the test data
+test_data = scaled_data[training_data_len - 60:]
+X_test, y_test = [], dataset[training_data_len:]
+
+
+for i in range(60, len(test_data)):
+    X_test.append(test_data[i-60:i, 0])
+    
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1 ))
+
+# Make a Prediction
+# Predict using the trained LSTM model
+predictions = model.predict(X_test)
+predictions = scaler.inverse_transform(predictions)
+
+
+# Plotting data
+train = data[:training_data_len]
+test =  data[training_data_len:]
+
+test = test.copy()
+
+test['Predictions'] = predictions
+
+# Visualize actual vs predicted prices
+plt.figure(figsize=(12,8))
+# Plot closing price to visualize trend
+plt.plot(train['date'], train['close'], label="Train (Actual)", color='blue')
+# Plot closing price to visualize trend
+plt.plot(test['date'], test['close'], label="Test (Actual)", color='orange')
+# Plot closing price to visualize trend
+plt.plot(test['date'], test['Predictions'], label="Predictions", color='red')
+plt.title("Our Stock Predictions")
+plt.xlabel("Date")
+plt.ylabel("Close Price")
+plt.legend()
+plt.show()
